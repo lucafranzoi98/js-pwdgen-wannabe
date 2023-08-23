ada5ffa4492974666a19207e2048d9f7f5b51701
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-23 17:10:30 +0200

    bonus part

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,11 @@ const userSurname = prompt('Inserisci il tuo cognome:');
 
 const userColour = prompt('Inserisci il tuo colore preferito:');
 
-const pswUser = `${userName}${userSurname}${userColour}23`;
+const userKey = Number(prompt('Inserisci un numero chiave:'));
+
+const secretKey = userKey + 23;
+
+const pswUser = `${userName}${userSurname}${userColour}${secretKey}`;
 
 console.log(pswUser);
 
